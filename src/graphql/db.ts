de17618e89132f270655f747edd4f14e870bffcb
myunghyun_yang <shiningy84@gmail.com>
2021-01-23 17:50:20 +0900

    add movie functions

--- src/graphql/db.ts
+++ src/graphql/db.ts
@@ -1,39 +1,51 @@
-const people = [
+let movies = [
   {
-    id: "0",
-    name: "Nicolas",
-    age: 18,
-    gender: "female",
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "1",
-    name: "Eric",
-    age: 18,
-    gender: "male",
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "2",
-    name: "Jessy",
-    age: 24,
-    gender: "female",
+    id: 2,
+    name: "The Godfather I",
+    score: 99
   },
   {
-    id: "3",
-    name: "Austin",
-    age: 20,
-    gender: "male",
-  },
-  {
-    id: "4",
-    name: "Sean",
-    age: 30,
-    gender: "male",
-  },
+    id: 3,
+    name: "Logan",
+    score: 2
+  }
 ];
 
-const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+export const getMovies = () => movies;
+
+export const getById = id => {
+  const filteredMovie = movies.filter(movie => movie.id === id);
+  return filteredMovie[0];
 }
 
-export {people, getById};
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id : movies.length + 1,
+    name, 
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+}
+
+// export {getMovies, getById, deleteMovie};
