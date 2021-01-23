1c55dad56d9731db73242594597b0b97aa8710d9
myunghyun_yang <shiningy84@gmail.com>
2021-01-24 00:59:49 +0900

    update schema, resolvers and db

--- src/graphql/db.ts
+++ src/graphql/db.ts
@@ -1,43 +1,45 @@
-import fetch from "node-fetch";
-const API_URL = "https://yts.am/api/v2/list_movies.json?";
+import axios from "axios";
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = (limit, rating) => {
-let REQUEST_URL = API_URL;
-if (limit > 0) {
-  REQUEST_URL += `limit=${limit}`;
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
 }
-if (rating > 0) {
-  REQUEST_URL += `&minimum_rating=${rating}`;
-}
-
-  return fetch(`${REQUEST_URL}`)
-    .then((res) => res.json())
-    .then((json) => json.data.movies);
-}
-
-// export const getById = id => {
-//   const filteredMovie = movies.filter(movie => movie.id === id);
-//   return filteredMovie[0];
-// }
-
-// export const deleteMovie = (id) => {
-//   const cleanedMovies = movies.filter(movie => movie.id !== id);
-//   if (movies.length > cleanedMovies.length) {
-//     movies = cleanedMovies;
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// export const addMovie = (name, score) => {
-//   const newMovie = {
-//     id : movies.length + 1,
-//     name,
-//     score
-//   };
-//   movies.push(newMovie);
-//   return newMovie;
-// }
-
-// export {getMovies, getById, deleteMovie};
